e369ac1cac8b8650a9bb79c69c3964ff12420942
Amos Lima <social.amos@gmail.com>
2020-08-26 18:32:05 -0300

    localhost

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
+  host: '127.0.0.1:5432',
   username: 'hztvcfjveknilx',
   password: 'bbeba8b3337c9ac14426debefa7c9d36cf62a523b5eb8c20651c87f0f2dbbb13',
   database: 'codonto-quiz',
