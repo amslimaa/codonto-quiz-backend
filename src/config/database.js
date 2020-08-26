252b38b764dfa48a0e3e6bdb2d44dc679a5a05c3
Amos Lima <social.amos@gmail.com>
2020-08-26 18:23:13 -0300

    config pg

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   dialect: 'postgres',
   host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
+  username: 'hztvcfjveknilx',
+  password: 'bbeba8b3337c9ac14426debefa7c9d36cf62a523b5eb8c20651c87f0f2dbbb13',
   database: 'codonto-quiz',
   define: {
     timestamps: true,
